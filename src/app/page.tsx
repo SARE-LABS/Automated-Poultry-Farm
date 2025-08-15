454c065e574091af432d163b3ccc9e64844fab56
Emmytweg <emmanuelolamide706@gmail.com>
2025-08-15 01:02:33 +0100

    feat: Implement responsive sidebar and top navbar with mobile menu support
    - Added a new Sidebar component with navigation items and responsive design.
    - Integrated a TopNavbar component that includes user greeting, notifications, and a mobile menu.
    - Enhanced user experience with dynamic greeting based on time of day.
    - Introduced utility functions for class name management using clsx and tailwind-merge.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,22 @@
-import React from 'react'
+'use  client'
 
-function page() {
+import { TopNavbar } from "@/components/TopNavbar"
+import  {SideNavbar}  from "@/components/Sidebar"
+import { OverviewCards } from "@/components/OverviewCards"
+
+export default function Dashboard() {
   return (
-    <div className='font-bold'>SARE</div>
+    <div className="flex md:h-screen bg-gray-50 overflow-hidden">
+      <SideNavbar />
+      <div className="flex-1 flex flex-col">
+        <TopNavbar />
+        <main className="flex-1 p-6">
+          <div className="mb-6">
+            <h2 className="text-2xl font-semibold text-gray-900 mb-2">Overview</h2>
+          </div>
+          <OverviewCards />
+        </main>
+      </div>
+    </div>
   )
 }
-
-export default page
