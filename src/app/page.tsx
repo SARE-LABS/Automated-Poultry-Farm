358f3da985d7f45972386c1d158f1f6fc0e1b22d
Kennedy Ifeadi <kennedyifeadi@gmail.com>
2025-08-19 07:26:13 +0100

    feat: remove AirHealthChart component from page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,10 @@
 import { AirHealthChart } from '@/components/airHealth/AirHealthChart'
+import { ParameterCard } from '@/components/airHealth/ParameterCard'
 import React from 'react'
 
 function page() {
   return (
-    <div className=''>
-      <AirHealthChart />
-    </div>
+    <div className=''></div>
   )
 }
 
